68964f7c84873170904b67b5cb941acb87ccaedc
Ed Cavazos <wayo.cavazos@gmail.com>
2020-12-17 10:59:54 -0800

    TagHelpers.Input - some refactoring

--- TagHelpers.fs
+++ TagHelpers.fs
@@ -31,21 +31,17 @@ type Input =
     static member Of([<ReflectedDefinition>] expr: Expr<'a>, attrs_a: XmlAttribute list) =
         match expr with
         | PropInfo(property_info, get_current_value) ->
-
-            let current_value = get_current_value()
-
+            
             let mutable ls : XmlAttribute list = []
-                        
-            let property_name = property_info.Name
-            
-            let mutable display_name = property_info.Name
-            
-            let _ =
+                                    
+            let display_name =
 
                 let cattr = System.Attribute.GetCustomAttribute(property_info, typedefof<DisplayAttribute>) :?> DisplayAttribute
 
                 if (not (isNull cattr)) then
-                    display_name <- cattr.Name
+                    cattr.Name
+                else
+                    property_info.Name
 
             // System.Console.WriteLine(property_name + " : " + property_info.PropertyType.Name)
 
@@ -91,12 +87,12 @@ type Input =
 
                 if (not (isNull cattr)) then
                     if (cattr.MinimumLength > 0) then
-                        ls <- ls @ [ attr "data-val-length" (sprintf "The field %s must be a string with a minimum length of %i and a maximum length of %i." property_name cattr.MinimumLength cattr.MaximumLength) ]
+                        ls <- ls @ [ attr "data-val-length" (sprintf "The field %s must be a string with a minimum length of %i and a maximum length of %i." property_info.Name cattr.MinimumLength cattr.MaximumLength) ]
                         ls <- ls @ [ attr "data-val-length-max" (string cattr.MaximumLength) ]
                         ls <- ls @ [ attr "data-val-length-min" (string cattr.MinimumLength) ]
                         ls <- ls @ [ attr "maxlength" (string cattr.MaximumLength) ]                        
                     else 
-                        ls <- ls @ [ attr "data-val-length" (sprintf "The field %s must be a string with a maximum length of %i." property_name cattr.MaximumLength) ]
+                        ls <- ls @ [ attr "data-val-length" (sprintf "The field %s must be a string with a maximum length of %i." property_info.Name cattr.MaximumLength) ]
                         ls <- ls @ [ attr "data-val-length-max" (string cattr.MaximumLength) ]
                         ls <- ls @ [ attr "maxlength" (string cattr.MaximumLength) ]        
 
@@ -106,7 +102,7 @@ type Input =
 
                 if (not (isNull cattr)) then
 
-                    ls <- ls @ [ attr "data-val-regex" (sprintf "The field %s must match the regular expression %s." property_name cattr.Pattern) ]
+                    ls <- ls @ [ attr "data-val-regex" (sprintf "The field %s must match the regular expression %s." property_info.Name cattr.Pattern) ]
                     ls <- ls @ [ attr "data-val-regex-pattern" cattr.Pattern ]
 
             let _ =
@@ -115,7 +111,7 @@ type Input =
 
                 if (not (isNull cattr)) then
 
-                    ls <- ls @ [ attr "data-val-range" (sprintf "The field %s must be between %s and %s." property_name (string cattr.Minimum) (string cattr.Maximum)) ]
+                    ls <- ls @ [ attr "data-val-range" (sprintf "The field %s must be between %s and %s." property_info.Name (string cattr.Minimum) (string cattr.Maximum)) ]
                     ls <- ls @ [ attr "data-val-range-max" (string cattr.Maximum) ]
                     ls <- ls @ [ attr "data-val-range-min" (string cattr.Minimum) ]        
             
@@ -124,7 +120,7 @@ type Input =
                 let cattr = System.Attribute.GetCustomAttribute(property_info, typedefof<RequiredAttribute>) :?> RequiredAttribute
 
                 if (not (isNull cattr)) then
-                    ls <- ls @ [ attr "data-val-required" (sprintf "The %s field is required." property_name) ]
+                    ls <- ls @ [ attr "data-val-required" (sprintf "The %s field is required." property_info.Name) ]
 
             let attrs_b = 
                 [
@@ -135,17 +131,17 @@ type Input =
             
             let attrs_d = 
                 [
-                    _id property_name
-                    _name property_name
+                    _id property_info.Name
+                    _name property_info.Name
                     _value
                         (
                             if type_value = "date" then
-                                if (isNull current_value) then
+                                if (isNull (get_current_value())) then
                                     ""
                                 else
-                                    (current_value :?> System.DateTime).ToString "yyyy-MM-dd"
+                                    (get_current_value() :?> System.DateTime).ToString "yyyy-MM-dd"
                             else
-                                (string current_value)
+                                (string (get_current_value()))
                         )
 
                 ]                                 
